9813e8d002433f4a91af6b78f4938d61dfba8b49
thanhtam98 <thanhtam98.ng@gmail.com>
2022-10-06 22:23:11 +0700

    Create dev_containner

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (void)
+{
+    cout << "Hello woasdasdasrld " << endl;
+    cout << "Hello world " << endl;
+    cout << "Hello wodasdasdrld " << endl;
+    cout << "Hello world " << endl;
+    cout << "Hello asdasdworld " << endl;
+    cout << "Hello world " << endl;
+    cout << "Hello world " << endl;
+    cout << "Hello world " << endl;
+
+}
