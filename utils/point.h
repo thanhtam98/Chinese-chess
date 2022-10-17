b241a7b1bade3df11ba76009c274f9065d36ec67
thanhtam98 <thanhtam98.ng@gmail.com>
2022-10-17 21:18:46 +0700

    Add game logic (amended)

--- utils/point.h
+++ utils/point.h
@@ -4,13 +4,13 @@
 
 class Point {
 public:
-    Point *of(int _x, int _y);
+    static Point *of(int _x, int _y);
     team_code getTeam();
 private:
     Point(int _x, int _y): x(_x), y(_y) {};
     int x;
     int y;
-    Point* cache[BOARD_LENGTH*BOARD_WIDTH];
+    static Point* cache[BOARD_LENGTH*BOARD_WIDTH];
 };
 
 #endif
