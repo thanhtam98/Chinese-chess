2e982cbd463b09a8489ce74dca4c4d42606fc40d
Nguyen Duy Tan <duytan2804@gmail.com>
2023-05-09 22:34:30 +0700

    Change the signal bars

--- ui/label/teamSignalLabels.cpp
+++ ui/label/teamSignalLabels.cpp
@@ -6,7 +6,7 @@
 TeamSignalLabels::TeamSignalLabels(FWidget* parent) {
     team_code turn_team = ITurn::getTurn()->getTeam();
     upper = new FLabel{parent};
-    upper->setText("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+    upper->setText("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
     upper->setForegroundColor(turn_team == BLACK ? BLACK_BG : RED_BG);
     upper->setGeometry(
         FPoint{OFFSET_X, OFFSET_Y-1},
@@ -14,7 +14,7 @@ TeamSignalLabels::TeamSignalLabels(FWidget* parent) {
     );
 
     lower = new FLabel{parent};
-    lower->setText("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+    lower->setText("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
     lower->setForegroundColor(turn_team == BLACK ? BLACK_BG : RED_BG);
     lower->setGeometry(
         FPoint{OFFSET_X, BOARD_LENGTH*SPACE_BW_PIECE_Y+OFFSET_Y-1},
