f3592b9dea7709fb579804d6e7f3464d25f7494a
X1C7 <thanhtam98.ng@gmail.com>
2023-10-30 21:28:42 +0700

    add server/client examle (not working)

--- main.cpp
+++ main.cpp
@@ -8,16 +8,24 @@
 #include "board.h"
 #include "mainDialog.h"
 #include "log.h"
+#include "websockpp.h"
 
 using namespace std;
 using namespace finalcut;
 
+auto webSockHandler(void)
+{
+      utility_server s;
+      s.run();
+}
 auto main (int argc, char* argv[]) -> int
 {
   // freopen("/workspaces/chinese-chess/log.txt","w",stdout);
   // cout<<"Start the logfile" << endl;
   string str = "Alo!";
   LOG_F("Start the game %s", str.c_str());
+  std::thread thread_object (webSockHandler);
+
   FApplication app{argc, argv};
 
   // Force terminal initialization without calling show()
