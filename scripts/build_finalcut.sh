b07e8c86f154ca548328197397e284aaf90cdebb
ntt <ntt@localhost.localdomain>
2023-11-02 20:20:25 -0400

    optimize build time

--- scripts/build_finalcut.sh
+++ scripts/build_finalcut.sh
@@ -5,11 +5,11 @@ rm -rf libfinal
 sudo apt-get update -y
 sudo apt-get install -y autotools-dev automake autoconf autoconf-archive libtool pkg-config
 mkdir libfinal
-git clone https://github.com/gansm/finalcut.git
+git clone --depth 1 --branch 0.9.0 https://github.com/gansm/finalcut.git 
 cd finalcut
 autoreconf --install --force
 ./configure --prefix=$(realpath ../libfinal)
-make
+make -j$(nproc)
 sudo make install
 sudo chmod -R 777 $(realpath ../libfinal)
 mv ../libfinal/include/final ../
