052165842c0b840282349da8e2af7a2ae673c32d
Nguyen Duy Tan <duytan2804@gmail.com>
2023-12-10 11:06:51 +0700

    [UI] Change label texts of mode selection radio

--- ui/label/inc/modeSelectionRadioButton.h
+++ ui/label/inc/modeSelectionRadioButton.h
@@ -11,11 +11,11 @@ public:
     void selectMode();
     void addDebugTrick();
 private:
-    const std::string ONLINE = "ONLINE"; 
+    const std::string ONLINE = "Online"; 
     // const std::string ONLINE_DESC = "Each player on an individual computer.";
-    const std::string OFFLINE = "OFFLINE"; 
+    const std::string OFFLINE = "Offline"; 
     // const std::string OFFLINE_DESC = "2 players on the same computer";
-    const std::string DEBUG = "DEBUG"; 
+    const std::string DEBUG = "Debug"; 
     // const std::string DEBUG_DESC = "2 players moving freely withou turn\nrestriction";
     const std::string MODE_SELECTION_LABEL = "Please choose a mode";
 
